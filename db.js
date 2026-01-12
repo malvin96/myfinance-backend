cd49c9f58fdc7a2f1a6c9c4aa0d6109182880f23
malvin96 <malvin.hendrawan@gmail.com>
2026-01-12 12:06:46 +0800

    Refactor transactions table in db.js
    Removed unnecessary fields from the transactions table and adjusted the table structure.

--- db.js
+++ db.js
@@ -5,21 +5,14 @@ export const db = new Database("myfinance.db")
 db.exec(`
 CREATE TABLE IF NOT EXISTS transactions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  telegram_id TEXT,
-  user_code TEXT,
+  chat_id TEXT,
+  user TEXT,
   type TEXT,
-  account TEXT,
-  asset TEXT,
-  category TEXT,
-  need_type TEXT,
   amount INTEGER,
+  account TEXT,
+  category TEXT,
   note TEXT,
   tags TEXT,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS dictionaries (
-  key TEXT PRIMARY KEY,
-  value TEXT
 );
 `)
