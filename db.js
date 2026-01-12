2e4c43dad19f649d48b0d1872958c29fa616f755
malvin96 <malvin.hendrawan@gmail.com>
2026-01-12 12:45:57 +0800

    Modify transactions table to include tags and timestamp

--- db.js
+++ db.js
@@ -5,18 +5,14 @@ export const db = new Database("finance.db")
 db.exec(`
 CREATE TABLE IF NOT EXISTS transactions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  chat_id TEXT,
   user TEXT,
   account TEXT,
   category TEXT,
   note TEXT,
   amount INTEGER,
   type TEXT,
-  created_at TEXT
-);
-
-CREATE TABLE IF NOT EXISTS balances (
-  user TEXT,
-  account TEXT,
-  balance INTEGER
+  tags TEXT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 `)
